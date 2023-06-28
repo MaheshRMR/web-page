6b6c2ebed32f67e953c1baf1d567645e0c204722
MaheshRMR <rameshandramesh11@gmail.com>
2023-06-28 10:43:32 +0530

    ff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
 
-class HelloWorld extends React.Component {
-  render() {
-    return <h1>Hello, World!</h1>;
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  )
 }
-
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
